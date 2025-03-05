16dc222b1e04b82c42c01776c3b0a03c11727474
Lucas Falcão <lucas.olifalco@gmail.com>
2025-03-05 05:09:06 -0300

    update

--- BaldeTinta.py
+++ BaldeTinta.py
@@ -10,10 +10,9 @@ def preenchimentoBFS(imagem, linha, coluna, nova_cor):
     fila = deque([(linha, coluna)])
     imagem[linha][coluna] = nova_cor
 
-    # Vizinhança 8-conectada (horizontal, vertical e diagonal)
     direcoes = [
-        (-1, 0), (1, 0), (0, -1), (0, 1),  # Vizinhos ortogonais
-        (-1, -1), (-1, 1), (1, -1), (1, 1)  # Vizinhos diagonais
+        (-1, 0), (1, 0), (0, -1), (0, 1),
+        (-1, -1), (-1, 1), (1, -1), (1, 1)
     ]
 
     while fila:
@@ -28,7 +27,6 @@ def preenchimentoBFS(imagem, linha, coluna, nova_cor):
 
     return imagem
 
-# Função para ler a matriz de um arquivo TXT
 def ler_matriz_do_arquivo(nome_arquivo="entrada.txt"):
     with open(nome_arquivo, "r") as arquivo:
         linhas = arquivo.readlines()
@@ -39,28 +37,22 @@ def ler_matriz_do_arquivo(nome_arquivo="entrada.txt"):
 
     return matriz
 
-# Função para ler o ponto inicial e a nova cor
 def entrada_parametros():
     linha, coluna = map(int, input("Informe as coordenadas do ponto inicial (linha, coluna): ").split())
     nova_cor = int(input("Informe a nova cor: "))
     return linha, coluna, nova_cor
 
-# Função para salvar a matriz em um arquivo TXT
 def salvar_matriz_em_txt(matriz, nome_arquivo="saida.txt"):
     with open(nome_arquivo, "w") as arquivo:
         for linha in matriz:
             arquivo.write(" ".join(map(str, linha)) + "\n")
 
-# Lê a matriz do arquivo de entrada
 imagem = ler_matriz_do_arquivo()
 
-# Lê os parâmetros do preenchimento
 linha, coluna, nova_cor = entrada_parametros()
 
-# Aplica a ferramenta Balde de Tinta usando BFS com vizinhança 8-conectada
 imagem_resultante = preenchimentoBFS(imagem, linha, coluna, nova_cor)
 
-# Salva a matriz resultante no arquivo TXT
 salvar_matriz_em_txt(imagem_resultante)
 
 print(f"Matriz resultante salva em 'saida.txt'.")
--- conversores/txt-img.py
+++ conversores/txt-img.py
@@ -5,5 +5,5 @@ arr = np.loadtxt('../saida.txt', dtype=np.uint8)
 
 img = Image.fromarray(arr, mode='L')
 
-img.save('imgs/imagem_reconstruida.png')
+img.save('../imgs/imagem_reconstruida.png')
 img.show()
